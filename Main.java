d611d35ee3eaaf4c09576c7b7a934864701f6b89
Ashutosh Kumar <singhashutosh0609@gmail.com>
2025-12-26 00:04:31 +0530

    first commit

---
+++ Main.java
@@ -0,0 +1,20 @@
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Stack<String> stack = new Stack<String>();
+
+        stack.push("Valorant");
+        stack.push("BGMI");
+        stack.push("COC");
+        stack.push("Badminton");
+        stack.push("Cricket");
+
+        //String myFavGame = stack.pop();
+
+        //System.out.println(stack);
+        //System.out.println(stack.peek());
+        System.out.println(stack.search("BGMI"));
+    }
+}
