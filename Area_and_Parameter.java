3489426f888e5ace267e53cb06aeb6d95977ba10
Ashutosh Kumar <singhashutosh0609@gmail.com>
2026-01-03 15:57:41 +0000

    Jan 3, 2026, 9:27 PM

--- Area_and_Parameter.java
+++ Area_and_Parameter.java
@@ -1,10 +1,26 @@
 import java.util.*;
 public class Area_and_Parameter 
 {
-    
+    static long area(long Len, long Bre)
+    {
+        return Len*Bre;
+    }
+
+    static long Parameter(long Len, long Bre)
+    {
+        return 2*(Len+Bre);
+    }
     public static void main (String[] args)
     {
         System.out.println("Enter Length : ");
+        Scanner Len = new Scanner(System.in);
+        System.out.println("Enter Bredth : ");
+        Scanner Bre = new Scanner(System.in);
+        
+
+
+
+        /*System.out.println("Enter Length : ");
         Scanner Length = new Scanner(System.in);
         int Len = Length.nextInt();
 
@@ -13,7 +29,7 @@ public class Area_and_Parameter
         int Br = Breadth.nextInt();
 
         System.out.println("Area = " + Len*Br);
-        System.out.println("Parameter = " + (2*Len+2*Br) );
+        System.out.println("Parameter = " + (2*Len+2*Br) );*/
     }
 
 }
