57506de37c23406c2546b9cea22aa51a1f96f406
Ashutosh Kumar <singhashutosh0609@gmail.com>
2026-01-03 17:02:40 +0000

    Jan 3, 2026, 10:32 PM

--- Swap_Two_numbers.java
+++ Swap_Two_numbers.java
@@ -1,5 +1,27 @@
 import java.util.*;
 public class Swap_Two_numbers
 {
-       
+   static int[] number(int a, int b)
+   {
+        int[] Number=new int[5];
+        
+        Number[0]=a;
+        Number[1]=b;
+        return Number ;
+   } 
+   
+   public static void main(String[] args)
+   {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("Enter 1st number :");
+    int a = sc.nextInt();
+
+    System.out.println("Enter 2nd number : ");
+    int b = sc.nextInt();
+
+    int[] Display= number(a,b);
+
+    System.out.println("Swapped number is " +Display[1] + "  " + Display[0]);
+   }
 }
