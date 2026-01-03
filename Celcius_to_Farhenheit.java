c274cc845dcbe563a038a50211b225c420c1c63c
Ashutosh Kumar <singhashutosh0609@gmail.com>
2026-01-03 16:48:09 +0000

    Jan 3, 2026, 10:18 PM

--- Celcius_to_Farhenheit.java
+++ Celcius_to_Farhenheit.java
@@ -1,4 +1,16 @@
+import java.util.*;
 public class Celcius_to_Farhenheit
 {
-     
+     static double Temp (double Degree)
+     {
+        return 32+(Degree*9/5);
+     }
+
+     public static void main(String[] args)
+     {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter temperature in Celcius : ");
+        double Degree = sc.nextDouble();
+        System.out.println("Temperature in Farhenheit is : " + Temp (Degree));
+     }
 }
---
+++ Swap_Two_numbers.java
@@ -0,0 +1,5 @@
+import java.util.*;
+public class Swap_Two_numbers
+{
+       
+}
