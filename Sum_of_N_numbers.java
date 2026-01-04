27e6a117d2d67cec640a7ff5d3ad08b8972ececb
Ashutosh Kumar <singhashutosh0609@gmail.com>
2026-01-04 09:52:08 +0000

    Jan 4, 2026, 3:22 PM

---
+++ Sum_of_N_numbers.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Sum_of_N_Numbers
+{
+    static int Addition(int[] Store)
+
+    {
+        int Sum =0;
+        for(int j=0; j<Store.length; j++)
+        {
+            Sum += Store[j];
+        }
+        return Sum;
+    }
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the count of numbers you want the sum of : ");
+        int count = sc.nextInt();
+
+        int[] Store= new int[count];
+        for(int i=0; i<count; i++)
+        {
+            System.out.println("enter number : ");
+            Store[i] = sc.nextInt();
+            sc.nextLine();
+        }
+        System.out.println("The Sum of all numbers is : " + Addition(Store));
+
+    }
+}
