0b208102fef60c364feb4d3f3904e3ce5d95fae9
Ashutosh Kumar <singhashutosh0609@gmail.com>
2026-01-10 15:58:51 +0000

    Jan 10, 2026, 9:28 PM

---
+++ Pattern_Printing.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Pattern_Printing 
+{
+    static void Pattern(int n)
+    {
+        for(int i = 1; i<=n; i++)
+        {
+            for(int j=0; j<=i; j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Pattern(n);
+    }   
+}
